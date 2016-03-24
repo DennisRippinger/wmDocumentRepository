c96730920b806ec5d44ffd6f9a8c8b045c7cedd5
Dennis Rippinger <dennis.rippinger@gmail.com>
2016-03-24 19:58:11 +0100

    Added Banner

---
+++ serviceExtractor/src/main/java/de/drippinger/serviceExtractor/Banner.java
@@ -0,0 +1,36 @@
+/**
+ * Copyright (C) 2016 Dennis Rippinger (dennis.rippinger@msg-systems.com)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package de.drippinger.serviceExtractor;
+
+/**
+ * Banner
+ *
+ * @author Dennis Rippinger
+ */
+public final class Banner {
+
+	public static void printBanner() {
+
+		System.out.println("  _________                  .__               ___________         __                        __                ");
+		System.out.println(" /   _____/ ______________  _|__| ____  ____   \\_   _____/__  ____/  |_____________    _____/  |_  ___________ ");
+		System.out.println(" \\_____  \\_/ __ \\_  __ \\  \\/ /  |/ ___\\/ __ \\   |    __)_\\  \\/  /\\   __\\_  __ \\__  \\ _/ ___\\   __\\/  _ \\_  __ \\");
+		System.out.println(" /        \\  ___/|  | \\/\\   /|  \\  \\__\\  ___/   |        \\>    <  |  |  |  | \\// __ \\\\  \\___|  | (  <_> )  | \\/");
+		System.out.println("/_______  /\\___  >__|    \\_/ |__|\\___  >___  > /_______  /__/\\_ \\ |__|  |__|  (____  /\\___  >__|  \\____/|__|   ");
+		System.out.println("        \\/     \\/                    \\/    \\/          \\/      \\/                  \\/     \\/                   ");
+
+		// TODO Add documentation link
+	}
+}
